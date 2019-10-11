b3a2cc310099a56d96faad7159f535091d6dc776
Stanley Ulili <stanleyulili@gmail.com>
2019-10-11 12:28:25 +0200

    (feat): show weather info from api when page loads

--- script.js
+++ script.js
@@ -1,10 +1,21 @@
-const res = fetch('http://api.openweathermap.org/data/2.5/weather?q=zomba&APPID=35b1f1d45a7b4378cf2430ae601816be');
+const weather = document.querySelector('.weather');
+const url = 'http://api.openweathermap.org/data/2.5/weather?q=zomba&APPID=35b1f1d45a7b4378cf2430ae601816be&units=metric';
 
-res
-  .then((data) => data.json())
-  .then((weatherData) => {
-    console.log(weatherData.weather);
+fetch(url)
+  .then((response) => response.json())
+  .then((data) => {
+    const markup = `<h1 class="location">${data.name}, ${data.sys.country}</h1>
+ <div class="weather__summary">
+    <p><i class="wi wi-cloud weather-icon"></i> <span class="weather__celsius-value">${data.main.temp}°C</span></p>
+    <p>${data.weather[0].main}</p>
+    <ul class="weather__miscellaneous">
+    <li><i class="wi wi-humidity"></i> Humidity  <span>${data.main.humidity}%</span></li>
+    <li><i class="wi wi-small-craft-advisory"></i> Wind Speed <span>${data.wind.speed} m/s</span></li>
+    </ul>
+ </div>
+ `;
+    weather.insertAdjacentHTML('beforeend', markup);
   })
-  .catch((err) => {
-    console.log(err);
+  .catch((error) => {
+    console.log(error);
   });
