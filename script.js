670dcb63ea2ebe6d4030106152ca23b47f4de5c6
Stanley Ulili <stanleyulili@gmail.com>
2019-10-11 20:18:47 +0200

    (feat): get forecast and log each object in the console.
    It does not yeat render the forecasts in html

--- script.js
+++ script.js
@@ -2,6 +2,21 @@ const weather = document.querySelector('.weather');
 const searchBtn = document.querySelector('button[role="search"]');
 const cityField = document.querySelector('input[type="search"]');
 
+function renderForecast(forecast) {
+  forecast.forEach((weatherData) =>{
+    console.log(weatherData);
+  });
+}
+
+function getForecast(city) {
+  const url = `http://api.openweathermap.org/data/2.5/forecast?q=${city}&APPID=35b1f1d45a7b4378cf2430ae601816be&units=metric`;
+  fetch(url)
+  .then((response) => response.json())
+  .then((data) => {
+    const forecastData = data.list.filter((obj) => obj.dt_txt.endsWith('06:00:00'));
+    renderForecast(forecastData);
+  })
+}
 function getCityWeather(city) {
   const url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&APPID=35b1f1d45a7b4378cf2430ae601816be&units=metric`;
 
